9b2fd4a34d37d384aa701714e1546e5e7dc15647
Daniela <dannytorres0211@gmail.com>
2022-05-13 11:04:42 -0500

    Modified App.js with request-axios to the mision

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  //GET
+  //pedir muchos recursos
+  axios
+    .get("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      console.log("Proceso finalizado");
+    });
+
+    //pedir un recursos especifico (id)
+  axios
+    .get("https://jsonplaceholder.typicode.com/posts/1")
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      console.log("Proceso finalizado");
+    });
+
+  //POST
+  //crear un recurso por primera vez
+  axios
+    .post(
+      "https://jsonplaceholder.typicode.com/users",
+      {
+        id: 11,
+        name: "Daniela",
+        userusername: "Danny",
+        email: "dannytorres@gmail.com"
+      },
+      {
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    )
+    .then((response) => {
+      console.log("response", response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      console.log("Proceso finalizado");
+    });
+
+  //PUT
+  axios
+    .put(
+      "https://jsonplaceholder.typicode.com/posts/1",
+      {
+        id: 1,
+        title: 'Hello',
+        body: 'World and People',
+        userId: 1,
+      },
+      {
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    )
+    .then((response) => {
+      console.log("response", response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      console.log("Proceso finalizado");
+    });
+
+  //DELETE
+  axios
+    .delete("https://jsonplaceholder.typicode.com/users/1")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        console.log("Proceso finalizado");
+      });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ejercicio de request con axios</h1>
     </div>
   );
 }
